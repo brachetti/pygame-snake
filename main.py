abfaf792c7d5b7831b6d4066152eb0b4f66646e1
Ronald Brachetti <brachial.software@gmail.com>
2022-08-23 23:30:06 +0200

    change game area size

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from mimetypes import init
 import sys
 import pygame
 
 FRAMERATE = 60
+CELL_SIZE = 40
+CELL_NUMBER = 20
+
+class Fruit:
+    def __init__(self) -> None:
+        # create an x and y position
+        self.x = 5
+        self.y = 4
+        # draw a square
+        pass
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+screen = pygame.display.set_mode((CELL_SIZE*CELL_NUMBER, CELL_SIZE*CELL_NUMBER))
 clock = pygame.time.Clock()
 screen.fill((175,215,70))
 
@@ -16,5 +27,6 @@ while True:
             sys.exit()
 
     # draw all our elements
+    
     pygame.display.update()
     clock.tick(FRAMERATE)
