4475ee8a68658bbaf1552c95a427d6bab6e55ea2
Ronald Brachetti <brachial.software@gmail.com>
2022-08-24 13:41:26 +0200

    move the snake

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cgi import print_form
+from curses import KEY_DOWN, echo
 from mimetypes import init
 import sys
 import pygame
@@ -35,14 +37,43 @@ class Snake:
             Vector2(6, 10),
             Vector2(7, 10),
         ]
-        self.blocks = [
+        self.direction = "up"
+    
+    def blocks(self):
+        return [
             Block(position, Snake.color)
             for position in self.body
         ]
-    
+
     def draw(self):
-        for block in self.blocks:
+        for block in self.blocks():
             block.draw_block()
+
+    def move_snake(self):
+        direction = Vector2(0,0)
+        if self.direction == "up":
+            direction = Vector2(0, -1)
+        if self.direction == "down":
+            direction = Vector2(0, 1)
+        if self.direction == "left":
+            direction = Vector2(-1, 0)
+        if self.direction == "right":
+            direction = Vector2(1, 0)
+        
+        body = self.body[:-1]
+        body.insert(0, body[0] + direction)
+        self.body = body[:]
+
+    def change_direction(self, event):
+        if event.key == pygame.K_UP and self.direction != "down":
+            self.direction = "up"
+        if event.key == pygame.K_DOWN and self.direction != "up":
+            self.direction = "down"
+        if event.key == pygame.K_LEFT and self.direction != "right":
+            self.direction = "left"
+        if event.key == pygame.K_RIGHT and self.direction != "left":
+            self.direction = "right"
+        
 
 class Fruit:
     color = (126, 166, 114)
@@ -60,7 +91,8 @@ class Fruit:
 pygame.init()
 screen = pygame.display.set_mode((CELL_SIZE*CELL_NUMBER, CELL_SIZE*CELL_NUMBER))
 clock = pygame.time.Clock()
-screen.fill(surface_bg)
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 fruit = Fruit()
 snake = Snake()
@@ -69,9 +101,16 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit()      
+
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+
+        if event.type == pygame.KEYDOWN:
+            snake.change_direction(event)
 
     # draw all our elements
+    screen.fill(surface_bg)
     fruit.draw()
     snake.draw()
     
